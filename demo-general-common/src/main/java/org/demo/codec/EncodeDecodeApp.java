aee23cacd795490d31b1036fcd2cea40b4a27646
gsskhan <g.s.s.khan@gmail.com>
2017-04-20 03:42:03 -0500

    updated project

--- demo-general-common/src/main/java/org/demo/codec/EncodeDecodeApp.java
+++ demo-general-common/src/main/java/org/demo/codec/EncodeDecodeApp.java
@@ -10,11 +10,14 @@ public class EncodeDecodeApp {
 	private static Logger log = LoggerFactory.getLogger(EncodeDecodeApp.class);
 	
 	public static void main(String[] args) {
+		
 		String str = "GULAM SABIR SHAHBAZ KHAN";
 		String encodedString = Base64.encodeBase64String(str.getBytes());
+		
 		log.info("Original = "+ str);
 		log.info("Encoded = " + encodedString);
 		log.info("Decoded = "+ new String(Base64.decodeBase64(encodedString)));
+		log.info("end of program ...");
 	}
 
 }
