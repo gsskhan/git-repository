5351fd0d74c7b89076f6ccf41ce226d33a06abf3
gsskhan <g.s.s.khan@gmail.com>
2017-10-21 21:52:44 -0500

    added DemoModule to main

--- vs-ang-app/src/main.ts
+++ vs-ang-app/src/main.ts
@@ -3,10 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { DemoModule } from "./demo/demo.module";
 
 if (environment.production) {
   enableProdMode();
 }
 
+/* commented default code - gulam 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+*/
+platformBrowserDynamic().bootstrapModule(DemoModule)
+.catch(err => console.log(err));
