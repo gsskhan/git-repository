200a323c307909da3526fd74ba61f218ba5681c0
gsskhan <g.s.s.khan@gmail.com>
2018-03-18 20:02:18 -0600

    message app - logging updated

--- demo-drools/src/main/java/com/demo/drools/main/AppMain1.java
+++ demo-drools/src/main/java/com/demo/drools/main/AppMain1.java
@@ -7,10 +7,14 @@ import org.kie.api.builder.KieModule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.internal.io.ResourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.demo.drools.model.Message;
 
 public class AppMain1 {
+	
+	private static Logger log = LoggerFactory.getLogger("AppMain1");
 	
 	public static void main(String[] args) {
 		
@@ -25,15 +29,18 @@ public class AppMain1 {
         KieContainer kieContainer = kieServices.newKieContainer(kieModule.getReleaseId());
         
         KieSession kieSession = kieContainer.newKieSession();
+        log.info("Kie session created...");
         
         Message msg = new Message();
-        msg.setText("Bye");
+        msg.setText("Hello");
+        
+        kieSession.setGlobal("log", log);
         
         kieSession.insert(msg);
         kieSession.fireAllRules();
         kieSession.dispose();
         
-        System.out.println("Program completed ...");
+        log.info("Program completed ...");
 		
 	}	
 
