5f494d2a8a15b8017a68620841c3673dbfb76df3
gsskhan <g.s.s.khan@gmail.com>
2018-08-18 01:33:00 -0500

    Adding spring state machine

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+
+Hints:
+
+1. To create a maven project.
+    mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
---
+++ demo-drools/src/main/java/com/demo/statemachine/main/AppMain3.java
@@ -0,0 +1,14 @@
+package com.demo.statemachine.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppMain3{
+
+    private static Logger log = LoggerFactory.getLogger("AppMain3");
+
+    public static void main(String[] args) {
+        log.info("State machine Demo program started...");
+    }
+
+}
