a0bdfcdeb1f887ac58acd673f2f66d0f1862f1d2
gsskhan <gsskhan@gsskhan-Inspiron-3542>
2019-09-30 00:10:09 +0530

    demo-spring-config initial commit

---
+++ demo-spring-config/src/main/java/org/demo/microservice/springconfig/DemoSpringConfigApplication.java
@@ -0,0 +1,13 @@
+package org.demo.microservice.springconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringConfigApplication.class, args);
+	}
+
+}
