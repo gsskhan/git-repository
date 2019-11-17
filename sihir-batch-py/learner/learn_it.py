fa0620702fee8951e18ea9eecf6509a30eed41b3
gsskhan <g.s.s.khan@gmail.com>
2019-11-18 01:27:41 +0530

    learn_it update 1

--- sihir-batch-py/learner/learn_it.py
+++ sihir-batch-py/learner/learn_it.py
@@ -4,7 +4,7 @@ def _display_initial_stmts():
     print("PYTHONPATH :" + str(sys.path))
     print("PYTHON binary location :"+sys.executable)
     pathname = os.path.dirname(sys.argv[0])
-    print("THIS executable script location :"+ os.path.abspath(pathname))
+    print("Script location :"+ os.path.abspath(pathname))
 
 
 if __name__ == "__main__":
