4d07aa0b761079f63658ab6282033a460599ebf5
gsskhan <g.s.s.khan@gmail.com>
2020-01-03 01:42:27 +0530

    updated dataframes.py

--- sihir-batch-py/src/learner/dataframes.py
+++ sihir-batch-py/src/learner/dataframes.py
@@ -13,13 +13,17 @@ print( "located python script [%s] at [%s]" %( main_script, exec_dir))
 students_json_file = src_dir + "/resources/students.json"
 
 with open(students_json_file) as data_file:
-    students_json = json.load(data_file)
-    print ( "Student json: \n", json.dumps(students_json, indent= 1))
+    students_json = json.load(data_file)    # read file and convert contents to json object
+print ( "Student json: \n", json.dumps(students_json, indent= 1))
+
 students_df1 = json_normalize (students_json)
 print ( "Students DataFrame using pandas.io.json_normalize(): \n %s" %(students_df1) )
 
 students_df2 = pd.read_json(students_json_file) 
 print ( "Students DataFrame using pandas.read_json(): \n %s" %(students_df2) )
 
+students_df3 = pd.DataFrame(students_json)
+print ( "Students DataFrame using pandas.DataFrame(): \n %s" %(students_df3) )
+    
 
 
