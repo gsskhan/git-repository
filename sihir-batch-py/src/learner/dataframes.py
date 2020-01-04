aefea991bee6ae05374cc5c13a2410b41b8dcd9c
gsskhan <g.s.s.khan@gmail.com>
2020-01-04 16:37:19 +0530

    pandas concat

--- sihir-batch-py/src/learner/dataframes.py
+++ sihir-batch-py/src/learner/dataframes.py
@@ -17,13 +17,18 @@ with open(students_json_file) as data_file:
 print ( "Student json: \n", json.dumps(students_json, indent= 1))
 
 students_df1 = json_normalize (students_json)
-print ( "Students DataFrame using pandas.io.json_normalize(): \n %s" %(students_df1) )
+print ( "Students DataFrame(1) using pandas.io.json_normalize(): \n %s" %(students_df1) )
 
 students_df2 = pd.read_json(students_json_file) 
-print ( "Students DataFrame using pandas.read_json(): \n %s" %(students_df2) )
+print ( "Students DataFrame(2) using pandas.read_json(): \n %s" %(students_df2) )
 
 students_df3 = pd.DataFrame(students_json)
-print ( "Students DataFrame using pandas.DataFrame(): \n %s" %(students_df3) )
+print ( "Students DataFrame(3) using pandas.DataFrame(): \n %s" %(students_df3) )
+
+frames = [students_df1, students_df2, students_df3]
+students_df = pd.concat(frames)
+print ( "Students DataFrame(concated all three) using pandas.concat(): \n %s" %(students_df) )
+
     
 
 
