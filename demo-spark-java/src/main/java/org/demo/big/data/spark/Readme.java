137891a203de9b1820bfe615ee7d289f0aedc2f2
gsskhan <g.s.s.khan@gmail.com>
2020-03-08 02:33:26 +0530

    updated readme.java

--- demo-spark-java/src/main/java/org/demo/big/data/spark/Readme.java
+++ demo-spark-java/src/main/java/org/demo/big/data/spark/Readme.java
@@ -1,10 +1,18 @@
 package org.demo.big.data.spark;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
 import org.demo.big.data.spark.conf.Spark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import scala.Tuple2;
 
 /**
  * Readme
@@ -17,22 +25,36 @@ public class Readme {
         JavaSparkContext sc = new Spark("Readme-Calculations", "local").getSparkContext();
         String file = sc.getSparkHome().get() + "/README.md";
 
-        JavaRDD<String> readmeDF = sc.textFile(file);
-        log.info("Total lines in {} file = {}", file, readmeDF.count());
-
-        log.info("Contents of readmeDF:");
-        readmeDF.foreach(l -> {
+        // Load the README.md file and display the contents
+        JavaRDD<String> readmeDS = sc.textFile(file);
+        log.info("Total lines in {} file = {}", file, readmeDS.count());
+        log.info("Contents of readmeDS:");
+        readmeDS.foreach(l -> {
             log.info(l);
         });
 
-        JavaRDD<String> linesHavingStringPythonDF = readmeDF.filter(l -> l.toLowerCase().contains("python"));
-        log.info("Contents of readmeDF:");
-        linesHavingStringPythonDF.foreach(l -> {
+        // Find the lines in file containing keyword - python
+        JavaRDD<String> linesHavingStringPythonDS = readmeDS.filter(l -> l.toLowerCase().contains("python"));
+        log.info("Contents of linesHavingStringPythonDS:");
+        linesHavingStringPythonDS.foreach(l -> {
             log.info(l);
         });
 
+        // Split up into words.
+        JavaRDD<String> wordsDS = readmeDS.flatMap(l -> {
+            Iterator<String> list = Arrays.asList(l.split(" ")).iterator();
+            return list;
+        });
+
+        // Transform into pairs and count.
+        JavaPairRDD<String, Integer> countsDS = wordsDS
+                                .mapToPair(s-> new Tuple2<>(s, 1))
+                                .reduceByKey( (Function2<Integer, Integer, Integer>) (a, b) -> a + b);
+
+        countsDS.saveAsTextFile("/tmp/word_count_of_readme_md");
+
+        // Destroy spark context
         sc.stop();
-
     }
 
 }
