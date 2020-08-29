c408087dd1031a6c77d8622a74a8af97a5f6388d
gsskhan <g.s.s.khan@gmail.com>
2020-08-29 19:26:27 +0530

    php demo app commit

---
+++ php-projects/demo-app/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Demonstration Application - Home</title>
+    <meta charset = "UTF-8" />
+</head>
+<body>
+<div>
+
+    <form action="welcome.php" method="post">
+    <table>
+    <tr><td>E-mail</td><td><input type="email" name="email" required></td></tr>
+    <tr><td>Password</td><td><input type="password" name="psword" required></td></tr>
+    </table>
+    <input type="submit">
+    </form>
+</div>
+</body>
+</html>
---
+++ php-projects/demo-app/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Demonstration Application - Welcome</title>
+    <meta charset = "UTF-8" />
+</head>
+<body>
+
+<div>Welcome <?php echo $_POST["email"]; ?></div>
+<div>Your password is: <?php echo $_POST["psword"]; ?></div>
+
+</body>
+</html>
