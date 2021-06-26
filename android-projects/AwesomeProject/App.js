593cf2b6229d03374e91d95dba1e2f8d2b3c794b
gsskhan <g.s.s.khan@gmail.com>
2021-06-27 02:40:57 +0530

    Updated AwsomeProject with react native

--- android-projects/AwesomeProject/App.js
+++ android-projects/AwesomeProject/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hello World! by GK.</Text>      
+      <Text> Hello World! by gsskhan</Text>      
       <StatusBar style="auto" />
     </View>
   );
