39c2d57a5e316c835c55cd3c9b61bbebf6830feb
gsskhan <g.s.s.khan@gmail.com>
2021-11-05 12:57:44 +0530

    Initial commit dms api

---
+++ java-projects/dms-api/src/main/java/org/dms/web/api/DmsApiApplication.java
@@ -0,0 +1,13 @@
+package org.dms.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsApiApplication.class, args);
+	}
+
+}
