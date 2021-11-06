630c0a9a9410a368ac65c5bf0b73239b07fa53b6
gsskhan <g.s.s.khan@gmail.com>
2021-11-06 22:58:01 +0530

    dms-ui with primeng initial commit

---
+++ nodejs-projects/dms-ui/src/app/app-primeng.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+/**
+ * @author GSSKHAN
+ * 
+ * Module for all used Prime NG modules in application.
+ * 
+ */
+
+@NgModule({
+  declarations: [],
+  exports: [
+
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppPrimengModule { }
