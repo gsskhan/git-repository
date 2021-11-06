a12543a5f2b65c53ba369d74371fe2d3ef38f383
gsskhan <g.s.s.khan@gmail.com>
2021-11-07 00:12:28 +0530

    Added Header

--- nodejs-projects/dms-ui/src/app/app.module.ts
+++ nodejs-projects/dms-ui/src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations"; 
-import { AppPrimengModule } from "./app-primeng.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PrimengModule } from "./primeng/primeng.module";
+import { HeaderComponent } from './header/header.component';
+import { LoginRegisterComponent } from './login-register/login-register.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppPrimengModule
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- nodejs-projects/dms-ui/src/app/primeng/primeng.module.ts
+++ nodejs-projects/dms-ui/src/app/primeng/primeng.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ToolbarModule } from "primeng/toolbar";
 
 /**
- * @author GSSKHAN
+ * @author G S S KHAN
  * 
- * Module for all used Prime NG modules in application.
+ * Application specific used PrimeNG modules.
  * 
  */
+
 
 @NgModule({
   declarations: [],
   exports: [
-
+    ToolbarModule
   ],
   imports: [
     CommonModule
   ]
 })
-export class AppPrimengModule { }
+export class PrimengModule { }
