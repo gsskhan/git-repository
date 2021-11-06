c882500e221ade726b65e9e41afa732f0e4765f5
gsskhan <g.s.s.khan@gmail.com>
2021-11-06 20:48:14 +0530

    Added header

--- nodejs-projects/dms-ui/src/app/app.module.ts
+++ nodejs-projects/dms-ui/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from "./app-material.module";
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
