726e2b55fad4d8ce9053803723c55bfc32c81fb0
gsskhan <g.s.s.khan@gmail.com>
2022-10-22 19:03:25 +0530

    Javers Added

--- java-projects/demo-javers/src/main/java/com/demo/javers/controllers/StoreController.java
+++ java-projects/demo-javers/src/main/java/com/demo/javers/controllers/StoreController.java
@@ -1,9 +1,15 @@
 package com.demo.javers.controllers;
 
+import java.util.*;
+
+import org.javers.core.Javers;
+import org.javers.core.metamodel.object.CdoSnapshot;
+import org.javers.repository.jql.QueryBuilder;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import com.demo.javers.entities.*;
 import com.demo.javers.models.*;
 import com.demo.javers.services.*;
 
@@ -13,6 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(path = { "/store" })
 public class StoreController {
+	
+	@Autowired
+	private Javers javers;
 
 	@Autowired
 	private StoreService storeService;
@@ -24,5 +33,12 @@ public class StoreController {
 		log.debug("Store controller ... method addStore completed. Response Data: {}", data);
 		return data;
 	}
+	
+	@GetMapping("/snapshots")
+    public String getStoresSnapshots() {
+        QueryBuilder jqlQuery = QueryBuilder.byClass(Store.class);
+        List<CdoSnapshot> snapshots = javers.findSnapshots(jqlQuery.build());
+        return javers.getJsonConverter().toJson(snapshots);
+    }
 
 }
--- java-projects/demo-javers/src/main/java/com/demo/javers/repositories/StoreRepository.java
+++ java-projects/demo-javers/src/main/java/com/demo/javers/repositories/StoreRepository.java
@@ -1,9 +1,11 @@
 package com.demo.javers.repositories;
 
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.javers.entities.Store;
 
+@JaversSpringDataAuditable
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }
