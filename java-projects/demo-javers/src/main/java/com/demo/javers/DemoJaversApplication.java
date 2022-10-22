38b47b7a6a0f974a97500eb2d25ebb04c6bdeda8
gsskhan <g.s.s.khan@gmail.com>
2022-10-22 12:21:27 +0530

    demo-javers initial commit

---
+++ java-projects/demo-javers/src/main/java/com/demo/javers/DemoJaversApplication.java
@@ -0,0 +1,13 @@
+package com.demo.javers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJaversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJaversApplication.class, args);
+	}
+
+}
---
+++ java-projects/demo-javers/src/test/java/com/demo/javers/DemoJaversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJaversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
