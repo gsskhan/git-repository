484a12caa082e77ef02a70c8aa9958c894cc8445
gsskhan <g.s.s.khan@gmail.com>
2023-07-09 13:26:43 +0530

    demo-jbpm-business-application ini

---
+++ java-projects/jbpm/demo-jbpm-business-application/src/main/java/org/demo/jbpm/app/web/DemoJbpmBusinessApplication.java
@@ -0,0 +1,13 @@
+package org.demo.jbpm.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJbpmBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJbpmBusinessApplication.class, args);
+	}
+
+}
---
+++ java-projects/jbpm/demo-jbpm-business-application/src/test/java/org/demo/jbpm/app/web/DemoJbpmBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.jbpm.app.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJbpmBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
