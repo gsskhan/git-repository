d4aa0c8fd0ca2b439d1c5f0487decfeac7c3af8d
gsskhan <g.s.s.khan@gmail.com>
2024-01-01 10:38:54 +0530

    AsyncConfig: Added comments

--- java-projects/demo-async/src/main/java/org/demo/async/app/config/AsyncConfig.java
+++ java-projects/demo-async/src/main/java/org/demo/async/app/config/AsyncConfig.java
@@ -2,15 +2,18 @@ package org.demo.async.app.config;
 
 import java.util.concurrent.Executor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Slf4j
 @Configuration
 public class AsyncConfig {
 
 	@Bean
 	public Executor taskExecutor() {
+		log.info("Configuring custom task executor.");
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		executor.setCorePoolSize(2);
 		executor.setMaxPoolSize(2);
