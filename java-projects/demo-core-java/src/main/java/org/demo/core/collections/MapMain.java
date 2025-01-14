2e0e4956b0c7868e026583c29bfcf5dcdf76e6b7
gsskhan <g.s.s.khan@gmail.com>
2025-01-14 20:18:08 +0530

    MapMain initial commit

--- java-projects/demo-core-java/src/main/java/org/demo/core/collections/MapMain.java
+++ java-projects/demo-core-java/src/main/java/org/demo/core/collections/MapMain.java
@@ -1,5 +1,6 @@
 package org.demo.core.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -9,15 +10,20 @@ public class MapMain {
 
     public static void main(String[] args) {
 
-        var mapOfIntegers = Map.of(1, "One", 2, "Two", 3, "Three", 4, "Four", 5, "Five");
+        var mapOfIntegers = new HashMap<>(Map.of(1, "One", 2, "Two", 3, "Three", 4, "Four", 5, "Five"));
         System.out.println("Original map of integers: " + mapOfIntegers);
 
-        var mapOfStrings = Map.of("One", 1, "Two", 2, "Three", 3, "Four", 4, "Five", 5);
+        var mapOfStrings = new HashMap<>(Map.of("One", 1, "Two", 2, "Three", 3, "Four", 4, "Five", 5));
         System.out.println("Original map of strings: " + mapOfStrings);
 
-        // Showcase all methods of Hashmap.
-        
-        
+        // showcase all methods of hashmap.
+        mapOfIntegers.putIfAbsent(6, "Six");
+        mapOfIntegers.computeIfPresent(7, (k, v) -> "Seven");
+        mapOfIntegers.computeIfPresent(7, (k, v) -> null);
+        mapOfIntegers.putIfAbsent(8, "Eight");
+        mapOfIntegers.computeIfAbsent(9, k -> null);
+        System.out.println("Modified map of integers (showcase 1)" + mapOfIntegers);
+
     }
 
 }
