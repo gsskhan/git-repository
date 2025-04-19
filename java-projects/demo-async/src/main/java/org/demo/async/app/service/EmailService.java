8155481579ba3770317e6cdfc7e134754c30c2e7
gsskhan <g.s.s.khan@gmail.com>
2025-04-19 18:33:10 +0530

    Refactor: Added Docstrings

--- java-projects/demo-async/src/main/java/org/demo/async/app/service/EmailService.java
+++ java-projects/demo-async/src/main/java/org/demo/async/app/service/EmailService.java
@@ -12,6 +12,16 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class EmailService {
 
+	/**
+	 * This method sends an email to the recipient asynchronously.
+	 *
+	 * @param toAddress
+	 *            the recipient's email address.
+	 * @param subjectText
+	 *            the email's subject.
+	 * @param bodyText
+	 *            the email's body.
+	 */
 	@Async
 	public void sendEmail(String toAddress, String subjectText, String bodyText) {
 		try {
@@ -24,12 +34,23 @@ public class EmailService {
 		}
 	}
 
+	/**
+	 * This method sends a html email to the recipient asynchronously.
+	 *
+	 * @param toAddress
+	 *            the recipient's email address.
+	 * @param subjectText
+	 *            the email's subject.
+	 * @param bodyText
+	 *            the email's body.
+	 * @return a CompletableFuture that completes with a boolean value indicating whether the email could be successfully sent.
+	 */
 	@Async
 	public CompletableFuture<Boolean> sendHtmlEmail(String toAddress, String subjectText, String bodyText) {
 		try {
 			log.info("sendHtmlEmail method start by {} at {}.", Thread.currentThread().getName(), LocalDateTime.now());
 			Thread.sleep(10000);
-			log.info("Mail sent to: [{}], Subject: [{}], Body [{}].", toAddress, subjectText, bodyText);
+			log.info("Html mail sent to: [{}], Subject: [{}], Body [{}].", toAddress, subjectText, bodyText);
 			log.info("sendHtmlEmail method ended by {} at {}.", Thread.currentThread().getName(), LocalDateTime.now());
 		} catch (Exception e) {
 			log.error("sendHtmlEmail method failed.", e);
