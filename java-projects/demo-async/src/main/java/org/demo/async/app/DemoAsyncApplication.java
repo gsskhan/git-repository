5e7fbc726307e893c484f803f16a1976ae4f6f48
gsskhan <g.s.s.khan@gmail.com>
2025-06-27 23:16:09 +0530

    Added java docs for DemoAsyncApplication

--- java-projects/demo-async/src/main/java/org/demo/async/app/DemoAsyncApplication.java
+++ java-projects/demo-async/src/main/java/org/demo/async/app/DemoAsyncApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * Main application class for the DemoAsyncApplication.
+ * This class enables asynchronous method execution using {@link EnableAsync}
+ * and is the entry point for the Spring Boot application.
+ *
+ */
 @EnableAsync
 @SpringBootApplication
 public class DemoAsyncApplication {
