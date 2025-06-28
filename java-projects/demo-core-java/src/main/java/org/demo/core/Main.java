12ad7b7824e9a78109acbb8b2732584faa207045
gsskhan <g.s.s.khan@gmail.com>
2025-06-28 12:22:28 +0530

    docs: Added java docs

--- java-projects/demo-core-java/src/main/java/org/demo/core/Main.java
+++ java-projects/demo-core-java/src/main/java/org/demo/core/Main.java
@@ -3,6 +3,12 @@ package org.demo.core;
 import org.demo.core.modules.PrintNumbers;
 
 public class Main {
+    /**
+     * The main method of the application.
+     * This method demonstrates the usage of the {@link PrintNumbers} class
+     * to print squared numbers and a halved value.
+     * @param args Command line arguments (not used in this application).
+     */
     public static void main(String[] args) {
     	PrintNumbers.printSquaredNumbers(1, 10);
         System.out.println("#############");
