c9b72ca0242479da79fc65b9da959a8abf8d193c
gsskhan <g.s.s.khan@gmail.com>
2025-10-11 21:59:50 +0530

    demo-spring-vaadin project initial commit

---
+++ java-projects/demo-spring-vaadin/src/main/java/org/demo/vaadin/DemoSpringVaadinApplication.java
@@ -0,0 +1,13 @@
+package org.demo.vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringVaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringVaadinApplication.class, args);
+	}
+
+}
