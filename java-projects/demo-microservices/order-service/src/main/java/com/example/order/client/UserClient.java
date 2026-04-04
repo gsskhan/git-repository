e1eaf023a602dfb2259488cd59a908c89a4a5246
gsskhan <g.s.s.khan@gmail.com>
2026-04-04 20:17:59 +0530

    fix: UserClient

--- java-projects/demo-microservices/order-service/src/main/java/com/example/order/client/UserClient.java
+++ java-projects/demo-microservices/order-service/src/main/java/com/example/order/client/UserClient.java
@@ -2,6 +2,7 @@ package com.example.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "USER-SERVICE")
 public interface UserClient {
