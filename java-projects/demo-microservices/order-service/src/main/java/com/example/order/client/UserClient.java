b29d9fa14838007fa16f20b219b2adde225d637d
gsskhan <g.s.s.khan@gmail.com>
2026-04-04 19:55:23 +0530

    impl order-service

--- java-projects/demo-microservices/order-service/src/main/java/com/example/order/client/UserClient.java
+++ java-projects/demo-microservices/order-service/src/main/java/com/example/order/client/UserClient.java
@@ -1,3 +1,10 @@
-public class UserClient {
-    
+package com.example.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "USER-SERVICE")
+public interface UserClient {
+    @GetMapping("/users/{id}")
+    String getUser(@PathVariable String id);
 }
--- java-projects/demo-microservices/order-service/src/main/java/com/example/order/controller/OrderController.java
+++ java-projects/demo-microservices/order-service/src/main/java/com/example/order/controller/OrderController.java
@@ -1,3 +1,22 @@
+package com.example.order.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.order.client.UserClient;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
-    
+
+    @Autowired
+    private UserClient userClient;
+
+    @GetMapping("/{id}")
+    public String getOrder(@PathVariable String id) {
+        return "Order for " + userClient.getUser(id);
+    }
 }
