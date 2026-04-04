573a1db08671e7cc5a15477416f82d316279ab17
gsskhan <g.s.s.khan@gmail.com>
2026-04-04 19:33:59 +0530

    Renamed packages

--- java-projects/demo-microservices/api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
+++ java-projects/demo-microservices/api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.apigateway;
+package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ java-projects/demo-microservices/common-lib/src/main/java/com/example/common/dto/UserDTO.java
@@ -0,0 +1,6 @@
+package com.example.common.dto;
+
+public class UserDTO {
+    private String id;
+    private String name;
+}
