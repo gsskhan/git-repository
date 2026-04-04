b088cee2100becc89be46662a0751b52e80d647c
gsskhan <g.s.s.khan@gmail.com>
2026-04-04 19:48:57 +0530

    user-service updates

--- java-projects/demo-microservices/user-service/src/main/java/com/example/user/controller/UserController.java
+++ java-projects/demo-microservices/user-service/src/main/java/com/example/user/controller/UserController.java
@@ -1,3 +1,17 @@
+package com.example.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-    
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable String id) {
+        return "User " + id;
+    }
 }
